c8feffa8828d5c0b4b2d9a8b1c67a14fd902f66d
vmathmachine <chrismaguschak@gmail.com>
2022-12-03 21:25:33 -0500

    Added new output message.

--- SalesTool2018/src/Main.java
+++ SalesTool2018/src/Main.java
@@ -10,5 +10,6 @@ public class Main {
 	private static void displayGreeting() {
 		System.out.println("HELLO HAPPY SALES PEOPLE!");
 		System.out.println("THIS APP SHOWS SALES DATA");
+		System.out.println("WOOT! IT PUSHED!");
 	}
 }
