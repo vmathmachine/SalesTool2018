7cb9abc9a7d72931fcf3a27ee1506b97dfcf418b
vmathmachine <chrismaguschak@gmail.com>
2022-12-03 20:44:30 -0500

    Initial project commit.

---
+++ SalesTool2018/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+	
+	public static void main(String[] args) {
+		SalesData data = new SalesData();
+		
+		displayGreeting();
+		data.display();
+	}
+	
+	private static void displayGreeting() {
+		System.out.println("HELLO HAPPY SALES PEOPLE!");
+		System.out.println("THIS APP SHOWS SALES DATA");
+	}
+}
---
+++ SalesTool2018/src/SalesData.java
@@ -0,0 +1,9 @@
+
+public class SalesData {
+	private int data[] = {0, 4, 42};
+	
+	public void display() {
+		System.out.println("TODO: Implement data display");
+	}
+	
+}
